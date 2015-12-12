6ea3345b63cd226a2ae3149a7c2e0c60a84d0b95
Weiming Zhai <wmzhai@gmail.com>
2015-12-12 09:08:46 +0800

    project settings

--- DepthBasics-Qt5/main.cpp
+++ DepthBasics-Qt5/main.cpp
@@ -1,11 +1,22 @@
 #include "stdafx.h"
 #include "DepthBasics.h"
 #include <QtWidgets/QApplication>
+#include "QKinectGrabber.h"
+#include "QImageWidget.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	DepthBasics w;
-	w.show();
+	//DepthBasics w;
+	//w.show();
+
+	QKinectGrabber k;
+	k.start();
+
+	QImageWidget colorWidget;
+	colorWidget.setMinimumSize(720, 480);
+	colorWidget.show();
+	QApplication::connect(&k, SIGNAL(colorImage(QImage)), &colorWidget, SLOT(setImage(QImage)));
+
 	return a.exec();
 }
