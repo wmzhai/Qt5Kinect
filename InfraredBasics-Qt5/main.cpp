c9d2c85ece4025c58e62dd47a76b43c9178b6859
Weiming Zhai <wmzhai@gmail.com>
2015-12-12 11:13:13 +0800

    project settings

--- InfraredBasics-Qt5/main.cpp
+++ InfraredBasics-Qt5/main.cpp
@@ -1,11 +1,24 @@
 #include "stdafx.h"
 #include "InfraredBasics.h"
 #include <QtWidgets/QApplication>
+#include "QKinectGrabber.h"
+#include "QImageWidget.h"
+
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	InfraredBasics w;
-	w.show();
+	//InfraredBasics w;
+	//w.show();
+
+	QKinectGrabber k;
+	k.start();
+
+	QImageWidget depthWidget;
+	depthWidget.setMinimumSize(512, 424);
+	depthWidget.show();
+	QApplication::connect(&k, SIGNAL(depthImage(QImage)), &depthWidget, SLOT(setImage(QImage)));
+
+
 	return a.exec();
 }
