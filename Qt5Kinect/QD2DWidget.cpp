fb458947d77f63866c2ae74840f444e623aab717
Weiming Zhai <wmzhai@gmail.com>
2015-12-14 17:31:16 +0800

    remove d2d header dependency

--- Qt5Kinect/QD2DWidget.cpp
+++ Qt5Kinect/QD2DWidget.cpp
@@ -4,20 +4,25 @@
 class QD2DWidgetPrivate
 {
 public:
-	UINT                     SourceHeight;
-	UINT                     SourceWidth;
-	LONG                     SourceStride;
+	ID2D1Factory*			D2DFactory;
+	ID2D1HwndRenderTarget*	RenderTarget;
+	ID2D1Bitmap*            D2DBitmap;
+	RGBQUAD*                ColorRGBX;
+	UINT                    SourceHeight;
+	UINT                    SourceWidth;
+	LONG                    SourceStride;
 };
 
 QD2DWidget::QD2DWidget(QWidget *parent, Qt::WindowFlags flags)
-	: QWidget(parent, flags), d_ptr(new QD2DWidgetPrivate),
-	m_pD2DFactory(NULL),
-	m_pHwndRenderTarget(NULL),
-	m_pBitmap(NULL)
+	: QWidget(parent, flags), d_ptr(new QD2DWidgetPrivate)
 
 {
 	setAttribute(Qt::WA_PaintOnScreen);
 	setAttribute(Qt::WA_NoSystemBackground);
+	d_ptr->D2DFactory = NULL;
+	d_ptr->RenderTarget = NULL;
+	d_ptr->D2DBitmap = NULL;
+	d_ptr->ColorRGBX = NULL;
 }
 
 QD2DWidget::~QD2DWidget()
@@ -36,10 +41,10 @@ HRESULT	QD2DWidget::Initialize()
 
 	
 	// create heap storage for color pixel data in RGBX format
-	m_pColorRGBX = new RGBQUAD[d_ptr->SourceWidth * d_ptr->SourceHeight];
+	d_ptr->ColorRGBX = new RGBQUAD[d_ptr->SourceWidth * d_ptr->SourceHeight];
 
 	// Create D2D factory
-	hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pD2DFactory);
+	hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &(d_ptr->D2DFactory));
 
 	D2D1_SIZE_U size = D2D1::SizeU(d_ptr->SourceWidth, d_ptr->SourceHeight);
 	D2D1_RENDER_TARGET_PROPERTIES rtProps = D2D1::RenderTargetProperties();
@@ -49,18 +54,18 @@ HRESULT	QD2DWidget::Initialize()
 	if (SUCCEEDED(hr))
 	{
 		// Create a Direct2D render target.
-		hr = m_pD2DFactory->CreateHwndRenderTarget(
+		hr = d_ptr->D2DFactory->CreateHwndRenderTarget(
 			D2D1::RenderTargetProperties(),
 			D2D1::HwndRenderTargetProperties((HWND)this->winId(), size),
-			&m_pHwndRenderTarget
+			&(d_ptr->RenderTarget)
 			);
 	}
 
 	// Create a bitmap that we can copy image data into and then render to the target
-	hr = m_pHwndRenderTarget->CreateBitmap(
+	hr = d_ptr->RenderTarget->CreateBitmap(
 		size,
 		D2D1::BitmapProperties(D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE)),
-		&m_pBitmap
+		&(d_ptr->D2DBitmap)
 		);
 
 	return hr;
@@ -70,26 +75,26 @@ HRESULT	QD2DWidget::Initialize()
 void QD2DWidget::Uninitialize()
 {
 
-	SAFE_RELEASE(m_pHwndRenderTarget);
-	SAFE_RELEASE(m_pD2DFactory);
+	SAFE_RELEASE(d_ptr->RenderTarget);
+	SAFE_RELEASE(d_ptr->D2DFactory);
 
-	if (m_pColorRGBX)
+	if (d_ptr->ColorRGBX)
 	{
-		delete[] m_pColorRGBX;
-		m_pColorRGBX = NULL;
+		delete[] d_ptr->ColorRGBX;
+		d_ptr->ColorRGBX = NULL;
 	}
 }
 
 
 void QD2DWidget::beginDraw()
 {
-	m_pHwndRenderTarget->BeginDraw();
+	d_ptr->RenderTarget->BeginDraw();
 }
 
 
 HRESULT QD2DWidget::endDraw()
 {
-	HRESULT hr = m_pHwndRenderTarget->EndDraw();
+	HRESULT hr = d_ptr->RenderTarget->EndDraw();
 	if (hr == D2DERR_RECREATE_TARGET)
 	{
 		Uninitialize();
@@ -99,28 +104,21 @@ HRESULT QD2DWidget::endDraw()
 }
 
 
-
-void QD2DWidget::ClearRenderTarget(D2D1::ColorF ClearColor)
-{
-	m_pHwndRenderTarget->Clear(ClearColor);
-}
-
-
 //-----------------------------------------------------------------------------
 // Name: render()
 // Desc: Draws the scene
 //-----------------------------------------------------------------------------
 HRESULT	QD2DWidget::render()
 {
-	if (!m_pHwndRenderTarget) return E_FAIL;
-	if ((m_pHwndRenderTarget->CheckWindowState() & D2D1_WINDOW_STATE_OCCLUDED)) return E_FAIL;
+	if (!d_ptr->RenderTarget) return E_FAIL;
+	if ((d_ptr->RenderTarget->CheckWindowState() & D2D1_WINDOW_STATE_OCCLUDED)) return E_FAIL;
 
 	HRESULT hr = S_OK;
 
 	beginDraw();
 
 	// Draw the bitmap stretched to the size of the window
-	m_pHwndRenderTarget->DrawBitmap(m_pBitmap);
+	d_ptr->RenderTarget->DrawBitmap(d_ptr->D2DBitmap);
 
 	endDraw();
 
@@ -131,7 +129,7 @@ void QD2DWidget::onResize(UINT nWidth, UINT nHeight)
 {
 	HRESULT hr = S_OK;
 
-	if (m_pHwndRenderTarget)
+	if (d_ptr->RenderTarget)
 	{
 		// Note: This method can fail, but it's okay to ignore the
 		// error here -- it will be repeated on the next call to
@@ -140,7 +138,7 @@ void QD2DWidget::onResize(UINT nWidth, UINT nHeight)
 		size.width = nWidth;
 		size.height = nHeight;
 
-		m_pHwndRenderTarget->Resize(size);
+		d_ptr->RenderTarget->Resize(size);
 	}
 	render();
 }
@@ -167,7 +165,7 @@ void QD2DWidget::resizeEvent(QResizeEvent *p_event)
 
 void QD2DWidget::setColorBuffer(const BYTE* pBuf)
 {
-	HRESULT hr = m_pBitmap->CopyFromMemory(NULL, pBuf, d_ptr->SourceStride);
+	HRESULT hr = d_ptr->D2DBitmap->CopyFromMemory(NULL, pBuf, d_ptr->SourceStride);
 	if (FAILED(hr))
 		return ;
 
--- Qt5Kinect/QD2DWidget.h
+++ Qt5Kinect/QD2DWidget.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include <d2d1.h>
-#include <d2d1helper.h>
-#include <dwrite.h>
-#include <wincodec.h>
-
-
 class QD2DWidgetPrivate;
 class QD2DWidget : public QWidget
 {
@@ -18,31 +12,21 @@ public:
 	HRESULT	Initialize();
 	void Uninitialize();
 	
-	void	ClearRenderTarget(D2D1::ColorF ClearColor);
-
 	virtual HRESULT	render();
-	void beginDraw();
-	HRESULT endDraw();
+
 
 	void onResize(UINT nWidth, UINT nHeight);
 
 protected:
 	QScopedPointer<QD2DWidgetPrivate> d_ptr;
+	void beginDraw();
+	HRESULT endDraw();
 
 public slots:
 	void setColorBuffer(const BYTE* pBuf);
-
 
 protected:
 	QPaintEngine *paintEngine() const { return 0; }
 	virtual void paintEvent(QPaintEvent *e);
 	virtual void resizeEvent(QResizeEvent *p_event);
-
-
-
-private:
-	ID2D1Factory*			m_pD2DFactory;
-	ID2D1HwndRenderTarget*	m_pHwndRenderTarget;
-	ID2D1Bitmap*            m_pBitmap;
-	RGBQUAD*                m_pColorRGBX;
 };
